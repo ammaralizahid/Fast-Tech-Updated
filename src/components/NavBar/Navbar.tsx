b9ca9e85d84443e0f41c4176fb666315ced4a3a7
ammaralizahid <amilzak42@gmail.com>
2023-12-23 06:20:40 +0500

    timer logic

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -22,112 +22,112 @@ const eye = <FontAwesomeIcon icon={faEye} />;
 import { BookOpenIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = ({ localSomething }: any) => {
-    let Links =[
-        {name:"Home",link:"/home"},
-        {name:"Orders",link:"/orders-history"},
-        {name:"About",link:"/"},
-        {name:"Contact",link:"/"},
-      ];
-      let [open, setOpen] =useState(false);
-      const [isLogin, setIsLogin] = useState(true);
-      const [showrestpassword, setShowrestpassword] = useState(false);
-      const [local, setLocalSomething] = useState<any>(localSomething || localStorage.getItem('cart_items'));
-      const history = useHistory();
-      const { isAuthenticated, login, logout } = useAuth();
-      const [token, setToken] = useState('');
-      const [loading, setLoading] = useState(false);
-      const [formData, setFormData] = useState({
-        email_or_phone: '',
-        password: '',
+  let Links = [
+    { name: "Home", link: "/home" },
+    { name: "Orders", link: "/orders-history" },
+    { name: "About", link: "/" },
+    { name: "Contact", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const [showrestpassword, setShowrestpassword] = useState(false);
+  const [local, setLocalSomething] = useState<any>(localSomething || localStorage.getItem('cart_items'));
+  const history = useHistory();
+  const { isAuthenticated, login, logout } = useAuth();
+  const [token, setToken] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email_or_phone: '',
+    password: '',
+  });
+
+  const changePath = () => {
+    setIsLogin(!isLogin);
+  };
+
+  const signInMutation = useSignIn();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    console.log({ name, value })
+
+    // Remove spaces, '-', and '()' for the input with name 'email_or_phone'
+    const sanitizedValue = name === 'email_or_phone' ? value.replace(/[\s()-]/g, '') : value;
+
+    setFormData((prevData: any) => ({
+      ...prevData,
+      [name]: sanitizedValue,
+    }));
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      const response = await signInMutation.mutateAsync({
+        ...formData,
+        restaurant_id: restaurantData.restaurant_id,
       });
 
-      const changePath = () => {
-        setIsLogin(!isLogin);
-      };
+      if (response.status === 200) {
+        toast.success('Logged In Successfully.');
+        login(response);
+        window.location.reload()
+      } else {
+        setLoading(false);
+      }
+    } catch (error: any) {
+      setLoading(false);
+      const { response } = error;
+      const { data } = response;
+      const { errors } = data;
 
-      const signInMutation = useSignIn();
+      toast.error(`${errors[0]?.message}`);
+    }
+  };
 
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+  const openrestpass = () => {
+    setShowrestpassword(!showrestpassword);
+  };
 
-        console.log({ name, value })
+  useEffect(() => {
+    const storedToken: any = localStorage.getItem('access_token');
+    setToken(storedToken);
+  }, [token, logout, login]);
 
-        // Remove spaces, '-', and '()' for the input with name 'email_or_phone'
-        const sanitizedValue = name === 'email_or_phone' ? value.replace(/[\s()-]/g, '') : value;
+  const logOut = () => {
+    logout();
+  };
 
-        setFormData((prevData: any) => ({
-          ...prevData,
-          [name]: sanitizedValue,
-        }));
-      };
-
-      const onSubmit = async (e: any) => {
-        e.preventDefault();
-        try {
-          setLoading(true);
-
-          const response = await signInMutation.mutateAsync({
-            ...formData,
-            restaurant_id: restaurantData.restaurant_id,
-          });
-
-          if (response.status === 200) {
-            toast.success('Logged In Successfully.');
-            login(response);
-            window.location.reload()
-          } else {
-            setLoading(false);
-          }
-        } catch (error: any) {
-          setLoading(false);
-          const { response } = error;
-          const { data } = response;
-          const { errors } = data;
-
-          toast.error(`${errors[0]?.message}`);
-        }
-      };
-
-      const openrestpass = () => {
-        setShowrestpassword(!showrestpassword);
-      };
-
-      useEffect(() => {
-        const storedToken: any = localStorage.getItem('access_token');
-        setToken(storedToken);
-      }, [token, logout, login]);
-
-      const logOut = () => {
-        logout();
-      };
-
-      const goToCheckout = () => {
-        if (localSomething && JSON.parse(localSomething)?.cart?.length) {
-          history.push('/cart');
-        }
-      };
+  const goToCheckout = () => {
+    if (localSomething && JSON.parse(localSomething)?.cart?.length) {
+      history.push('/cart');
+    }
+  };
 
 
 
 
 
-      const [passwordShown, setPasswordShown] = useState(false);
-      const togglePasswordVisiblity = () => {
-        setPasswordShown(passwordShown ? false : true);
-      };
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
 
-      const { register, handleSubmit } = useForm();
-      const onSubmitt = (e: any) => {
-        console.log(e);
-      };
-      const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const onSubmitt = (e: any) => {
+    console.log(e);
+  };
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-      const handleProfileClick = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-      };
+  const handleProfileClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-    return (
-      <div className='bg-black dark:bg-gray-900 fixed w-full z-[1000] lg-h-[23%] top-0 border-b border-gray-200 dark:border-gray-600'>
+  return (
+    <div className='bg-black dark:bg-gray-900 fixed w-full z-[1000] lg-h-[23%] top-0 border-b border-gray-200 dark:border-gray-600'>
       <div className='flex items-center bg-black justify-between py-4 md:px-10 px-7'>
         {/* logo section */}
         <div className='font-bold text-2xl  cursor-pointer text-white flex items-center gap-1'>
@@ -144,108 +144,101 @@ const Navbar = ({ localSomething }: any) => {
         </div>
         {/* Menu icon */}
         <div className='flex items-center '> {/* Updated this line */}
-       {token?(<>
+          {token ? (<>
 
-        <div className='flex mr-1'>
-            <div>
-              <FaShoppingCart className='text-white text-4xl cursor-pointer' onClick={goToCheckout} />
+            <div className='flex mr-1'>
+              <div>
+                <FaShoppingCart className='text-white text-4xl cursor-pointer' onClick={goToCheckout} />
+              </div>
+              <span className="bg-red-500 text-white text-xs rounded-full h-[19px] w-[10px] flex justify-center items-center px-2">{localSomething && JSON.parse(localSomething)?.cart?.length || 0}</span>
             </div>
-            <span className="bg-red-500 text-white text-xs rounded-full h-[19px] w-[10px] flex justify-center items-center px-2">{localSomething && JSON.parse(localSomething)?.cart?.length || 0}</span>
-          </div>
-          <FaHeart className='text-red-500 text-4xl cursor-pointer mr-2' onClick={() => history.push('/wishlist')} />
-          <CgProfile
-        id="avatarButton"
-        data-dropdown-toggle="userDropdown"
-        data-dropdown-placement="bottom-start"
-        className="w-10 h-10 rounded-full cursor-pointer text-white"
-        onClick={handleProfileClick}
-      />
+            <FaHeart className='text-red-500 text-4xl cursor-pointer mr-2' onClick={() => history.push('/wishlist')} />
+            <CgProfile
+              id="avatarButton"
+              data-dropdown-toggle="userDropdown"
+              data-dropdown-placement="bottom-start"
+              className="w-10 h-10 rounded-full cursor-pointer text-white"
+              onClick={handleProfileClick}
+            />
 
-      {isDropdownOpen && (
-        <div id="userDropdown"  className="cursor-pointer bg-black opacity-90 divide-y absolute -ml-[77px] mt-[210px]  divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-          <div className="px-4 py-3 text-sm text-white dark:text-white">
-            <div>Bonnie Green</div>
-            <div >name@flowbite.com</div>
-          </div>
+            {isDropdownOpen && (
+              <div id="userDropdown" className="cursor-pointer bg-black opacity-90 divide-y absolute -ml-[77px] mt-[210px]  divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <div className="px-4 py-3 text-sm text-white dark:text-white">
+                  <div>{restaurantData.name}</div>
+                  {/* <div >name@flowbite.com</div> */}
+                </div>
 
-          <div className="py-1">
-            <a className="block px-4 py-2 text-sm text-white hover:bg-green-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={openrestpass}>Reset Password</a>
-          </div>
-          <div className="py-1">
-            <a className="block px-4 py-2 text-sm text-white hover:bg-green-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={logOut}>Sign out</a>
-          </div>
-        </div>
-      )}
+                <div className="py-1">
+                  <a className="block px-4 py-2 text-sm text-white hover:bg-green-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={openrestpass}>Reset Password</a>
+                </div>
+                <div className="py-1">
+                  <a className="block px-4 py-2 text-sm text-white hover:bg-green-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={logOut}>Sign out</a>
+                </div>
+              </div>
+            )}
 
 
-       </>):(<>
+          </>) : (<>
 
-        <nav className="navbar border-radius-1 cursor-pointer navbar-dark text-center rounded-md bg-lime-500 sidebar-nav login-btn">
-                <div className="container-fluid">
-                  <h1
-                    className="navbar-toggler text-white font-semibold"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasDarkNavbar"
-                    aria-controls="offcanvasDarkNavbar"
-                    aria-label="Toggle navigation"
-                  >Login
-                  </h1>
-                  <div
-                    className="offcanvas offcanvas-end text-bg-#0D0D0D sidebar "
-                    tabIndex={-1}
-                    id="offcanvasDarkNavbar"
-                    aria-labelledby="offcanvasDarkNavbarLabel"
-                  >
-                    <div className="offcanvas-header bg-black text-white ">
-                      <button
-                        type="button"
-                        className="btn-close btn-close-green bg-White text-white border-10 z-50  text-4xl font-bold "
-                        data-bs-dismiss="offcanvas"
-                        aria-label="Close"
-                      >x</button>
-                    </div>
-                    {isLogin ? (
-                      <form onSubmit={onSubmit} className='bg-black h-[100%] p-3'>
-                        <div className="canvas-h1-div">
-                          <h1 className="canvas-h1">Join {restaurantData.name} Rewards, Win $500!</h1>
-                        </div>
-                        <div className="canvas-phone-div">
-                          <h1 className='canvas-phone-h1'>Email or Phone</h1>
-                          <InputMask
+            <nav className="navbar border-radius-1 cursor-pointer navbar-dark text-center rounded-md bg-lime-500 sidebar-nav login-btn">
+              <div className="container-fluid">
+                <h1
+                  className="navbar-toggler text-white font-semibold"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offcanvasDarkNavbar"
+                  aria-controls="offcanvasDarkNavbar"
+                  aria-label="Toggle navigation"
+                >Login
+                </h1>
+                <div
+                  className="offcanvas offcanvas-end text-bg-#0D0D0D sidebar "
+                  tabIndex={-1}
+                  id="offcanvasDarkNavbar"
+                  aria-labelledby="offcanvasDarkNavbarLabel"
+                >
+                  <div className="offcanvas-header bg-black text-white ">
+                    <button
+                      type="button"
+                      className="btn-close btn-close-green bg-White text-white border-10 z-50  text-4xl font-bold "
+                      data-bs-dismiss="offcanvas"
+                      aria-label="Close"
+                    >x</button>
+                  </div>
+                  {isLogin ? (
+                    <form onSubmit={onSubmit} className='bg-black h-[100%] p-3'>
+                      <div className="canvas-h1-div">
+                        <h1 className="canvas-h1">Join {restaurantData.name} Rewards, Win $500!</h1>
+                      </div>
+                      <div className="canvas-phone-div">
+                        <h1 className='canvas-phone-h1'>Email or Phone</h1>
+                        <InputMask
+                          className='canvas-phone-input'
+                          mask="+1(999) 999-9999"
+                          placeholder='+1 (555) 555-5555'
+                          type="text"
+                          name="email_or_phone"
+                          onChange={handleChange}
+                        />
+                        <div className="mt-3 mb-5">
+                          <h1 className='canvas-phone-h12'>Password</h1>
+                          <input
                             className='canvas-phone-input'
-                            mask="+1(999) 999-9999"
-                            placeholder='+1 (555) 555-5555'
-                            type="text"
-                            name="email_or_phone"
+                            placeholder='********'
+                            type={passwordShown ? "text" : "password"}
+                            name="password"
                             onChange={handleChange}
+
                           />
-                          <div className="mt-3 mb-5">
-                            <h1 className='canvas-phone-h12'>Password</h1>
-                            <input
-                              className='canvas-phone-input'
-                              placeholder='********'
-                              type={passwordShown ? "text" : "password"}
-                              name="password"
-                              onChange={handleChange}
+                          <i className='eye-icon text-lime-600 absolute mt-4' onClick={togglePasswordVisiblity}>{eye}</i>{" "}
 
-                            />
-                            <i className='eye-icon text-lime-600 absolute mt-4' onClick={togglePasswordVisiblity}>{eye}</i>{" "}
-
-
-
-                          </div >
-
-
-
-
-
-                          <p className='canvas-phone-p1 ml-3'>Don't have an account yet?<button className='canvas-phone-span' onClick={() => changePath()}>Sign up</button></p>
                         </div>
-                        <div className='mt-8'>
-                          <button className='canvas-footer-btn' type='submit'>Continue</button>
-                        </div>
+                        <p className='canvas-phone-p1 ml-3 mb-10'>Don't have an account yet?<button className='canvas-phone-span' onClick={() => changePath()}>Sign up</button></p>
+                      </div>
+                      <div className='flex items-center justify-center'>
+                        <button className='canvas-footer-btn' type='submit'>Continue</button>
+                      </div>
 
-                        {/* <div className="w-[100%] bg-black opacity-80">
+                      {/* <div className="w-[100%] bg-black opacity-80">
                             <div className="container">
                               <div className="row" >
                                 <div className="col-lg-4 canvas-footer-h1">
@@ -262,15 +255,15 @@ const Navbar = ({ localSomething }: any) => {
                             <button className='canvas-footer-btn' type='submit'>Continue</button>
                             <h1 className='canvas-footer-heading'>By signing up, you agree to receive email and SMS marketing communications from {restaurantData.name} and our technology partner Owner.com and consent to our Platform Terms and Privacy Policy.</h1>
                           </div> */}
-                      </form>
-                    ) : (
-                      <Singup changePath={changePath} />
-                    )}
-                  </div>
+                    </form>
+                  ) : (
+                    <Singup changePath={changePath} />
+                  )}
                 </div>
-              </nav>
+              </div>
+            </nav>
 
-       </>)}
+          </>)}
 
 
         </div>
@@ -279,7 +272,7 @@ const Navbar = ({ localSomething }: any) => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Navbar;
